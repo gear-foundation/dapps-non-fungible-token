dd2a5d138867ea56697db486e46339558681a129
Ivan Azoyan <azoyan.89@mail.ru>
2022-11-15 09:32:38 +0300

    add UpdateUser test

--- tests/nft_tests.rs
+++ tests/nft_tests.rs
@@ -1,3 +1,5 @@
+use std::time::{Duration, Instant};
+
 use gear_lib::non_fungible_token::delegated::DelegatedApproveMessage;
 use gear_lib::non_fungible_token::io::*;
 use gstd::{ActorId, Encode};
@@ -95,6 +97,40 @@ fn transfer_failures() {
     // must fail since transfer to the zero address
     transaction_id += 1;
     assert!(transfer(&nft, transaction_id, USERS[1], ZERO_ID, 0).main_failed());
+}
+
+#[test]
+fn update_user() {
+    let sys = System::new();
+    init_nft(&sys);
+    let nft = sys.get_program(1);
+    let mut transaction_id = 0u64;
+    assert!(!mint(&nft, transaction_id, USERS[0]).main_failed());
+    transaction_id += 1;
+    assert!(!approve(&nft, transaction_id, USERS[0], USERS[1], 0).main_failed());
+    let pair = Sr25519Pair::from_seed(&hex!(
+        "9d61b19deffd5a60ba844af492ec2cc44449c5697b326919703bac031cae7f60"
+    ));
+    let owner_id = pair.public().0;
+
+    let expires = (Instant::now() + Duration::from_secs(1))
+        .elapsed()
+        .as_secs();
+    let res = utils::update_user(
+        &nft,
+        USERS[1],
+        ActorId::from(owner_id),
+        0u64.into(),
+        expires,
+    );
+    println!("{:?}", res.decoded_log::<NFTEvent>());
+    let message = NFTEvent::UpdateUser {
+        token_id: 0.into(),
+        address: ActorId::from(owner_id),
+        expires,
+    }
+    .encode();
+    assert!(res.contains(&(USERS[1], message)));
 }
 
 #[test]
