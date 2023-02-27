2fa63d372c3d4fb9e7f4cbbc1a30029c16d6e072
EugeneWay <relax211@gmail.com>
2023-02-27 16:39:42 +0100

    add ui

---
+++ frontend/src/hooks/api.ts
@@ -0,0 +1,66 @@
+import { useAccount, useSendMessage } from '@gear-js/react-hooks';
+import { ADDRESS } from 'consts';
+import { Params, Token } from 'types';
+import { useParams } from 'react-router-dom';
+import stateMetaWasm from 'assets/wasm/state.meta.wasm';
+import metaTxt from 'assets/meta/meta.txt';
+import { useMetadata, useWasmMetadata } from './useMetadata';
+import { useReadWasmState } from './useReadWasmState';
+
+function useNFTMetadata() {
+  return useMetadata(metaTxt);
+}
+
+function useNFTState<T>(functionName: string, payload?: any) {
+  const { buffer } = useWasmMetadata(stateMetaWasm);
+
+  return useReadWasmState<T>(
+    ADDRESS.CONTRACT_ADDRESS,
+    buffer,
+    functionName,
+    payload,
+  );
+}
+
+function useNFT() {
+  const { id } = useParams() as Params;
+  const { state } = useNFTState<Token>('token', id);
+  console.log(state);
+  return state;
+}
+
+function useNFTs() {
+  const { state } = useNFTState<Token[]>('all_tokens');
+  return state;
+}
+
+function useOwnerNFTs() {
+  const { account } = useAccount();
+  const owner = account?.decodedAddress;
+
+  const { state, isStateRead } = useNFTState<Token[]>(
+    'tokens_for_owner',
+    owner,
+  );
+
+  return { ownerNFTs: state, isOwnerNFTsRead: isStateRead };
+}
+
+function useApprovedNFTs() {
+  const { account } = useAccount();
+  const decodedAddress = account?.decodedAddress;
+
+  const { state, isStateRead } = useNFTState<Token[]>(
+    'approved_tokens',
+    decodedAddress,
+  );
+
+  return { approvedNFTs: state, isApprovedNFTsRead: isStateRead };
+}
+
+function useSendNFTMessage() {
+  const meta = useNFTMetadata();
+  return useSendMessage(ADDRESS.CONTRACT_ADDRESS, meta);
+}
+
+export { useNFT, useNFTs, useOwnerNFTs, useApprovedNFTs, useSendNFTMessage };
